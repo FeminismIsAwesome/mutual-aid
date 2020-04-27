b1ecc423ac690449b2494931e6d88b809378712a
Howard M. Miller <hmiller@customink.com>
2020-04-26 23:15:16 -0400

    record nomenclature changes
    Co-authored-by: Mae <maebeale@users.noreply.github.com>

--- spec/javascript/pages/browse/ListBrowser.spec.js
+++ spec/javascript/pages/browse/ListBrowser.spec.js
@@ -6,10 +6,10 @@ it('works with reasonable data', function () {
   const testData = require('../../../../lib/listings.json')
   var wrapper = mount(ListBrowser, {
     propsData: {
-      listings: testData.listings,
+      listings: testData.contributions,
       helper: new ListingDataAdapter({
         categories: testData.categories,
-        locations: testData.locations,
+        locations: testData.service_areas,
       }),
     },
   })
